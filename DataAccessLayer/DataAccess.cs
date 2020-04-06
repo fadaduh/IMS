09e5c7734458e1892c7713803b884653f98f29bc
fadaduh <60125308+fadaduh@users.noreply.github.com>
2020-04-06 15:45:17 +0530

    HK: Commit 001

---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccessLayer
+{
+    public class DataAccess
+    {
+        public static DataTable SelectData(string query, List<SqlParameter> param)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connStr);
+
+            SqlCommand command = new SqlCommand(query, connection);
+            foreach (SqlParameter p in param)
+            {
+                command.Parameters.Add(p);
+            }
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            connection.Open();
+            adapter.Fill(dt);
+            connection.Close();
+
+            return dt;
+        }
+
+        public static int NonQuery(string query, List<SqlParameter> param)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connStr);
+
+            SqlCommand command = new SqlCommand(query, connection);
+            foreach (SqlParameter p in param)
+            {
+                command.Parameters.Add(p);
+            }
+
+            connection.Open();
+            int x = command.ExecuteNonQuery();
+            connection.Close();
+
+            return x;
+        }
+
+    }
+}
